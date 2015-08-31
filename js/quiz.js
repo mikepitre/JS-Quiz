00b6306093e95eaaf9a55064c98f3685925a3c96
Mike Pitre <michaelepitre@gmail.com>
2015-08-31 17:45:33 -0500

    added loop to ask question and compare user answer to correct answer, prints results to screen

--- js/quiz.js
+++ js/quiz.js
@@ -9,3 +9,18 @@ var questions = [
   ['What is the capital of California?', 'Sacramento']
 ];
 
+var correct = [];
+var incorrect = [];
+
+for (i = 0; i < questions.length; i++) {
+  var response = prompt(questions[i][0]);
+  if (response === questions[i][1]) {
+    correct.push(questions[i][0]);
+  } else {
+    incorrect.push(questions[i][0]);
+  }
+};
+
+print('You got ' + correct.length + ' question(s) right:' + correct);
+
+print('You got ' + incorrect.length + ' question(s) wrong:' + incorrect);
