96f48c28c019878f4cb385653f745ca1c792857c
Mike Pitre <michaelepitre@gmail.com>
2015-08-31 17:29:54 -0500

    added questions

--- js/quiz.js
+++ js/quiz.js
@@ -2,3 +2,10 @@
 function print(message) {
   document.write(message);
 }
+
+var questions = [
+  ['What is the capital of Texas?', 'Austin'],
+  ['What is the capital of NJ?', 'Trenton'],
+  ['What is the capital of California?', 'Sacramento']
+];
+
